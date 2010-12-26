94ca4510078f1ee0caa60c042da567ffd273559c
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2010-12-26 19:45:11 +0500

    - add slider

---
+++ apps/slider/migrations/0001_initial.py
@@ -0,0 +1,39 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Slide'
+        db.create_table('slider_slide', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('html', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True)),
+            ('order', self.gf('django.db.models.fields.IntegerField')(default=0)),
+        ))
+        db.send_create_signal('slider', ['Slide'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'Slide'
+        db.delete_table('slider_slide')
+
+
+    models = {
+        'slider.slide': {
+            'Meta': {'ordering': "['order']", 'object_name': 'Slide'},
+            'html': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'order': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        }
+    }
+
+    complete_apps = ['slider']
