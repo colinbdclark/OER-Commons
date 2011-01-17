2f52f67cf6bd70ff20f18a0aeaac2e4e445bf1b8
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-01-17 15:59:32 +0500

    use camel case for API methods URLs

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns("api.views",
    url("^search/?$", "search.search"),
-   url("^get_tags/?$", "get_tags.get_tags"),
-   url("^set_tags/?$", "set_tags.set_tags"),
-   url("^get_review/?$", "get_review.get_review"),
-   url("^set_review/?$", "set_review.set_review"),
+   url("^getTags/?$", "get_tags.get_tags"),
+   url("^setTags/?$", "set_tags.set_tags"),
+   url("^getReview/?$", "get_review.get_review"),
+   url("^setReview/?$", "set_review.set_review"),
 )
