f487a49f17794430f7e07fc237167c49671b86f1
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-01-27 10:49:39 +0500

    update RegistrationConfirmation admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -3,4 +3,19 @@ from django.contrib.admin.sites import site
 from users.models import RegistrationConfirmation
 
 
-site.register(RegistrationConfirmation, ModelAdmin)
+def user(obj):
+    user = obj.user
+    return "%s <%s>" % (user.username, user.email)
+
+
+class RegistrationConfirmationAdmin(ModelAdmin):
+
+    list_filter = ["confirmed", ]
+    list_display = [user, "key", "timestamp", "confirmed", ]
+    readonly_fields = ["user", "key", "timestamp", "confirmed"]
+
+    def has_add_permission(self, request):
+        return False
+
+
+site.register(RegistrationConfirmation, RegistrationConfirmationAdmin)
