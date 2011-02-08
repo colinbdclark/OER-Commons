49847d683e401348893b2b297030536b710efbf9
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-02-08 15:22:56 +0500

    - switch from jquery UI datepicker to DateInput plugin  - get rid of jquery UI

--- project/media/javascripts/content-submission.js
+++ project/media/javascripts/content-submission.js
@@ -125,5 +125,24 @@ oer.content_submission.init_rss_fields = function() {
       }
     }
   );
-  $("input[name='rss_timestamp_0']").datepicker();
+  $("input[name='rss_timestamp_0']").date_input();
 };
+
+$.extend(DateInput.DEFAULT_OPTS, {
+    stringToDate: function(string) {
+      var matches;
+      if (matches = string.match(/^(\d{2,2}).(\d{2,2}).(\d{4,4})$/)) {
+        return new Date(matches[3], matches[1] - 1, matches[2]);
+      } else {
+        return null;
+      };
+    },
+    
+    dateToString: function(date) {
+      var month = (date.getMonth() + 1).toString();
+      var dom = date.getDate().toString();
+      if (month.length == 1) month = "0" + month;
+      if (dom.length == 1) dom = "0" + dom;
+      return month + "." + dom + "." + date.getFullYear();
+    }
+});
