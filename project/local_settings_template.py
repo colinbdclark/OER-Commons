71f71a4da91a54c660b88f33b45d523282b4bc35
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-03-21 15:07:00 +0500

    - add local settings files to .gitignore  - add a template for local settings

---
+++ project/local_settings_template.py
@@ -0,0 +1,22 @@
+from project.settings import *
+
+DATABASES = {
+    'default': {
+        'NAME': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': '',
+        'PASSWORD': ''
+    },
+}
+
+
+ADMINS = (
+    # ('Your Name', 'your_email@domain.com'),
+)
+
+MANAGERS = ADMINS
+
+# Don't share this with anybody.
+SECRET_KEY = ''
+
+FLAVOR = "oercommons"
