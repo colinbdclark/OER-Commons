d7032bd2f61abaee6ae8f5f5d458d78c40eebb8d
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-03-25 19:15:03 +0500

    In autocomplete fields first show the items which start with input value, after that show item which contain the value.

--- apps/materials/views/autocomplete.py
+++ apps/materials/views/autocomplete.py
@@ -4,20 +4,27 @@ from django.shortcuts import get_object_or_404
 import cjson
 
 
+MAX_RESULTS = 10
+
+
 def autocomplete(request, app_label, model, field):
     content_type = get_object_or_404(ContentType, app_label=app_label,
                                      model=model)
     model = content_type.model_class()
-    
+
     results = []
     term = request.REQUEST.get("term", u"").strip()
     if term and len(term) >= 2:
         try:
-            kwargs = {"%s__icontains" % field: term}
-            results = model.objects.filter(**kwargs).values_list(field, flat=True).distinct().order_by(field)[:10]
+            kwargs = {"%s__istartswith" % field: term}
+            results = list(model.objects.filter(**kwargs).values_list(field, flat=True).distinct().order_by(field)[:MAX_RESULTS])
+            if len(results) < MAX_RESULTS:
+                kwargs = {"%s__icontains" % field: term}
+                exclude_kwargs = {"%s__in" % field: results}
+                results += list(model.objects.filter(**kwargs).exclude(**exclude_kwargs).values_list(field, flat=True).distinct().order_by(field)[:MAX_RESULTS - len(results)])
         except:
             pass
 
     results = [r.encode("utf-8") for r in results]
 
-    return HttpResponse(cjson.encode(results), "application/json") 
+    return HttpResponse(cjson.encode(results), "application/json")
