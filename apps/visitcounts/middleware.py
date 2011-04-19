62a51444c942eacb998864098b08c0020ac5cfdd
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-04-19 17:12:52 +0600

    Do not count robots as unique visitors

--- apps/visitcounts/middleware.py
+++ apps/visitcounts/middleware.py
@@ -1,4 +1,4 @@
-from visitcounts.models import UniqueVisitor
+from visitcounts.models import UniqueVisitor, ROBOT_RE
 
 
 COOKIE_NAME = "uniquevisitor"
@@ -9,6 +9,11 @@ AUTHENTICATED_COOKIE_VALUE = "1"
 class UniqueVisitorMiddleware(object):
     
     def process_response(self, request, response):
+
+        user_agent = request.META.get("HTTP_USER_AGENT")
+        if user_agent and ROBOT_RE.search(user_agent):
+            return response
+
         is_authenticated = request.user.is_authenticated()
         cookie_value = is_authenticated and AUTHENTICATED_COOKIE_VALUE or ANONYMOUS_COOKIE_VALUE
         if request.COOKIES.get(COOKIE_NAME, None) != cookie_value:
