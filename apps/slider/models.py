21e6d8e7eae15bdd47e3f0b4805cac20d6c9fb4f
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-04-19 21:19:33 +0600

    Add slider to microsite frontpage

--- apps/slider/models.py
+++ apps/slider/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.aggregates import Max
+from materials.models.microsite import Microsite
 
 
 class Slide(models.Model):
@@ -8,16 +9,19 @@ class Slide(models.Model):
     html = models.TextField(null=True, blank=True)
     image = models.ImageField(null=True, blank=True,
                               upload_to="upload/slider")
+    microsite = models.ForeignKey(Microsite, null=True, blank=True)
     order = models.IntegerField(default=0)
 
     def __unitode__(self):
         return self.title
 
     class Meta:
-        ordering = ["order"]
+        ordering = ["microsite", "order"]
 
     def save(self, *args, **kwargs):
         if not self.order:
-            max_order = Slide.objects.all().aggregate(Max("order"))["order__max"] or 0
+            max_order = Slide.objects.filter(
+                             microsite=self.microsite).aggregate(
+                                             Max("order"))["order__max"] or 0
             self.order = max_order + 1
         super(Slide, self).save(*args, **kwargs)
