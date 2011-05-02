824d626c3b89c03e37beb452d65fcb71c1c3be3d
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-05-02 14:08:48 +0600

    Remove obsolete rating javascript code from view resource page.

--- project/media/javascripts/materials-view-item.js
+++ project/media/javascripts/materials-view-item.js
@@ -7,44 +7,6 @@ oer.materials.view_item.init = function() {
       e.preventDefault();
       $navigation.find("form").attr("action", $(this).attr("href")).submit();
   }); 
-  
-  var $rate_form = $("form[name='rate']");
-  var $show_rate_form_button = $("div.view-item a.rate-item");
-  $show_rate_form_button.click(
-    function(e) {
-      e.preventDefault();
-      var $this = $(this);
-      $rate_form.find("select").val("5");
-      $rate_form.fadeIn(300);
-      $this.hide();
-    }
-  );
-  
-  $rate_form.find("a.cancel").click(
-    function(e) {
-      e.preventDefault();
-      $rate_form.hide();
-      $show_rate_form_button.show();
-    }
-  );
-  
-  $rate_form.find("a.rate").click(
-    function(e) {
-      e.preventDefault();
-      var $this = $(this);
-      oer.login.check_login(function() {
-        $.post($rate_form.attr("action"), {rating: $rate_form.find("select").val()},
-          function(data) {
-            var $stars = $this.closest("div.right").find("div.stars");
-            $stars.removeClass().addClass("stars").addClass(data.stars_class);
-            oer.status_message.success(data.message, true);
-            $rate_form.hide();
-            $show_rate_form_button.show();
-          });
-      });
-    }
-  );
-  
 };
 
 oer.materials.view_item.init_content_actions = function() {
