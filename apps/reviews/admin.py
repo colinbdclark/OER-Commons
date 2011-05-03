e666c5fe7b698004a99011221608f6161f4e885b
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-05-03 13:56:59 +0600

    Add links to resource to review admin.

--- apps/reviews/admin.py
+++ apps/reviews/admin.py
@@ -1,11 +1,23 @@
 from django.contrib.admin.options import ModelAdmin
 from django.contrib.admin.sites import site
 from reviews.models import Review
+from django.core.urlresolvers import reverse
 
 
 class ReviewAdmin(ModelAdmin):
 
-    list_display = ["__unicode__", "timestamp"]
+    def resource_link(self):
+        content_type = self.content_type
+        obj = content_type.model_class().objects.get(id=self.object_id)
+        admin_url = reverse("admin:%s_%s_change" % (content_type.app_label,
+                                                    content_type.model),
+                            args=(self.object_id,))
+        return """<a href="%s" target="_blank">on site</a> | <a href="%s" target="_blank">in admin</a>""" % (
+                                     obj.get_absolute_url(), admin_url)
+    resource_link.allow_tags = True
+    resource_link.short_description = u"View resource"
+        
+    list_display = ["__unicode__", resource_link, "timestamp",]
     readonly_fields = ["user", "content_type", "object_id"]
 
     def has_add_permission(self, request):
