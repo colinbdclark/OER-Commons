7ee3de363b0988b85d2ab0be8279648f5e620ac3
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-05-04 16:30:42 +0600

    Move Country model to "geo" app.

--- apps/materials/models/common.py
+++ apps/materials/models/common.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.db.models import permalink
 from django.db.models.fields import NOT_PROVIDED
 from django.utils.translation import ugettext_lazy as _
+from geo.models import Country
 from materials.ccrest import CcRest
 from south.modelsinspector import add_introspection_rules
 from urllib2 import HTTPError
@@ -223,34 +224,6 @@ class License(models.Model):
     def image(self):
         # TODO: fix this
         return self.image_url
-
-
-class CountryManager(models.Manager):
-    
-    def get_by_natural_key(self, code):
-        return self.get(code=code)
-
-
-class Country(models.Model):
-
-    name = models.CharField(unique=True, max_length=100,
-                            verbose_name=_(u"Name"))
-    slug = AutoSlugField(unique=True, max_length=100,
-                         populate_from="name",
-                         verbose_name=_(u"Slug"),
-                         db_index=True)
-    code = models.CharField(unique=True, max_length=2, db_index=True)
-
-    objects = CountryManager()
-
-    def __unicode__(self):
-        return self.name
-
-    class Meta:
-        app_label = "materials"
-        verbose_name = _(u"Country")
-        verbose_name_plural = _(u"Countries")
-        ordering = ("name",)
 
 
 class Author(models.Model):
