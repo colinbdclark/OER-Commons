cdf0e72bc55a899351c48abc8053a1d6dd287dff
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-05-18 17:07:55 +0600

    Allow to add multiple alignment tags.

--- project/media/javascripts/align-form.js
+++ project/media/javascripts/align-form.js
@@ -4,26 +4,42 @@ oer.align_form = {};
 
 oer.align_form.LOADING_EVENT = "oer-align-form-loading";
 oer.align_form.LOADED_EVENT = "oer-align-form-loaded";
-oer.align_form.SUBMITTED_EVENT = "oer-align-form-submitted";
 
+$.template("align-user-tags-item", '<li data-id="${id}"><a href="#" class="delete">Delete</a> <span>${code}</span></li>');
 
 oer.align_form.init = function() {
 
     var $form = $("#align-form");
 
-    var $buttons = $form.find("div.buttons");
+    var $user_tags = $("ul#align-user-tags");
+
+    $user_tags.delegate("a.delete", "click", function(e) {
+        e.preventDefault();
+        $this = $(this);
+        $li = $this.closest("li");
+        $li.fadeOut(250, function() {
+            $(this).detach();
+        });
+        var id = $li.data("id");
+        $.post($form.data("delete-url"), {
+            id : id
+        }, function(response, status, request) {
+        });
+    });
+
+    var $buttons = $form.find("#align-form-buttons");
     $buttons.find(":submit").button();
-    
+
     var $standard = $form.find("#id_curriculum_standard");
     var $standard_ct = $form.find("div.field.standard");
-    
+
     var $grade = $form.find("#id_curriculum_grade");
     var $grade_ct = $form.find("div.field.grade");
 
     var $category = $form.find("#id_curriculum_category");
     var $category_ct = $form.find("div.field.category");
 
-    var $tag= $form.find("#id_curriculum_tag");
+    var $tag = $form.find("#id_curriculum_tag");
     var $tag_ct = $form.find("div.field.tag");
 
     oer.align_form.init_dropdown($standard);
@@ -31,7 +47,7 @@ oer.align_form.init = function() {
     oer.align_form.init_dropdown($category);
 
     var $document = $(document);
-    
+
     $document.trigger(oer.align_form.LOADING_EVENT);
     $.post($standard.data("source"), function(data) {
         oer.align_form.load_options($standard, data);
@@ -46,20 +62,30 @@ oer.align_form.init = function() {
             $buttons.show();
         }
     });
-    
+
     $form.submit(function(e) {
         e.preventDefault();
         var value = $tag.val();
         if (value === "" || value === "-") {
             return;
         }
+        var code = $tag.find(":selected").data("code");
+        var $existing_tag = $user_tags.find("span:contains('" + code + "')").closest("li");
+        if ($existing_tag.length) {
+            $existing_tag.effect("bounce");
+            return;
+        }
         $document.trigger(oer.align_form.LOADING_EVENT);
-        $.post($form.attr("action"), {tag: value}, function(data) {
+        $.post($form.attr("action"), {
+            tag : value
+        }, function(data) {
+            if (data.status === "success") {
+                $.tmpl("align-user-tags-item", data.tag).hide().appendTo($user_tags).fadeIn(300); 
+            }
             $document.trigger(oer.align_form.LOADED_EVENT);
-            $document.trigger(oer.align_form.SUBMITTED_EVENT, data);
         });
     });
-    
+
 };
 
 oer.align_form.init_dropdown = function($dropdown) {
@@ -67,32 +93,34 @@ oer.align_form.init_dropdown = function($dropdown) {
     var $document = $(document);
     var $field = $dropdown.closest("div.field");
     var $next_fields = $field.nextAll("div.field");
-    var $next_dropdowns = $next_fields.find("select"); 
+    var $next_dropdowns = $next_fields.find("select");
     var $prev_fields = $field.prevAll("div.field");
     var $prev_dropdowns = $prev_fields.find("select");
     var $next_field = $next_fields.first();
     var $next_dropdown = $next_dropdowns.first();
+    var $buttons = $form.find("#align-form-buttons");
     $dropdown.change(function(e) {
-       var value = $dropdown.val();
-       if (value === "-") {
-           $next_fields.hide();
-           $next_dropdowns.empty();
-       } else {
-           var data = {};
-           data[$dropdown.attr("name")] = value;
-           $prev_dropdowns.each(function(i) {
-              var $d = $(this);
-              data[$d.attr("name")] = $d.val();  
-           });
-           $document.trigger(oer.align_form.LOADING_EVENT);
-           $field.addClass("loading");
-           $.post($next_dropdown.data("source"), data, function(data) {
-              oer.align_form.load_options($next_dropdown, data);
-              $next_field.show();
-              $document.trigger(oer.align_form.LOADED_EVENT);
-              $field.removeClass("loading");
-           });
-       }
+        var value = $dropdown.val();
+        $buttons.hide();
+        if (value === "-") {
+            $next_fields.hide();
+            $next_dropdowns.empty();
+        } else {
+            var data = {};
+            data[$dropdown.attr("name")] = value;
+            $prev_dropdowns.each(function(i) {
+                var $d = $(this);
+                data[$d.attr("name")] = $d.val();
+            });
+            $document.trigger(oer.align_form.LOADING_EVENT);
+            $field.addClass("loading");
+            $.post($next_dropdown.data("source"), data, function(data) {
+                oer.align_form.load_options($next_dropdown, data);
+                $next_field.show();
+                $document.trigger(oer.align_form.LOADED_EVENT);
+                $field.removeClass("loading");
+            });
+        }
     });
 };
 
@@ -124,9 +152,9 @@ oer.align_form.load_options = function($dropdown, data) {
 
 oer.align_form.reset = function() {
     var $form = $("#align-form");
-    
+
     $form.find("#id_curriculum_standard").val("-");
-    
+
     $form.find("#id_curriculum_grade").empty();
     $form.find("div.field.grade").hide();
 
@@ -135,6 +163,6 @@ oer.align_form.reset = function() {
 
     $form.find("#id_curriculum_tag").empty();
     $form.find("div.field.tag").hide();
-    
-    $form.find("div.buttons").hide();
+
+    $form.find("#align-form-buttons").hide();
 };
