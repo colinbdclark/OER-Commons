ba3d726daf1ff20868418ddf7dd3ce509c9acd56
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-05-24 10:42:33 +0600

    - reindex related resources after microsite topic is changed  - add "Recycling" keyword to "Tools for Change" topic  - move microsites fixtures to a separate file

--- apps/materials/tasks.py
+++ apps/materials/tasks.py
@@ -1,4 +1,5 @@
 from celery.decorators import task
+from haystack.query import SearchQuerySet
 import requests
 import urllib
 
@@ -25,3 +26,19 @@ def check_url_status(item):
         model.objects.filter(id=item.id).update(http_status=status_code)
         item = model.objects.get(id=item.id)
         site.update_object(item)
+
+
+@task
+def reindex_microsite_topic(topic):
+    from haystack.sites import site
+    
+    # get all objects from this topic and all objects with this topic's keywords
+    query = "topics:%s" % topic.id
+    keywords = topic.keywords.values_list("slug", flat=True)
+    if keywords:
+        query = "%s OR %s" % (query, " OR ".join(["keywords:%s" % kw for kw in keywords]))
+
+    for result in SearchQuerySet().narrow(query).load_all():
+        instance = result.object
+        site.update_object(instance)
+        
