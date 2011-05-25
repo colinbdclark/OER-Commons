fea7f6d4a118b88e40fe3d6ced406ca191300a51
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-05-25 16:18:55 +0600

    - add Preferences forms  - add "Show toolbar" option to preferences

--- project/settings.py
+++ project/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'preferences.middleware.PreferencesMiddleware',
     'depiction.middleware.ProfilerMiddleware',
     'django.middleware.cache.FetchFromCacheMiddleware',
 )
@@ -102,6 +103,7 @@ INSTALLED_APPS = (
     'harvester',
     'visitcounts',
     'newsletter',
+    'preferences',
     'project',
 )
 
