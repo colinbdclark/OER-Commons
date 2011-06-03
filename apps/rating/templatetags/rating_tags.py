a508c471145973e4609a1cbf72ce36db09195b5e
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-06-03 18:57:36 +0400

    Add microdata to individual resource page.

--- apps/rating/templatetags/rating_tags.py
+++ apps/rating/templatetags/rating_tags.py
@@ -1,3 +1,4 @@
+from django.db.models.aggregates import Avg
 from django.template import Library
 from rating import get_rating_stars_class
 
@@ -7,4 +8,16 @@ register = Library()
 
 @register.inclusion_tag("rating/include/stars.html")
 def stars(rating):
-    return {"class":get_rating_stars_class(rating)}
+    if isinstance(rating, float):
+        return {"class":get_rating_stars_class(rating)}
+    else:
+        ratings = rating.all()
+        count = ratings.count() 
+        value = 0.0 
+        if count:
+            value = ratings.aggregate(rating=Avg("value"))["rating"]
+
+        return {"class":get_rating_stars_class(value),
+                "rating_value": "%0.1f" % value,
+                "rating_count": count}
+        
