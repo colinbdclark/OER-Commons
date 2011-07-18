d5591a3667387951ff3189b5993bcbbe2ae43aa0
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-07-18 17:18:08 +0400

    Fix admin media prefix

--- project/settings.py
+++ project/settings.py
@@ -42,8 +42,8 @@ STATICFILES_PREPEND_LABEL_APPS = (
     'django.contrib.admin',
 )
 
-ADMIN_MEDIA_ROOT = os.path.join(STATIC_ROOT, 'admin_media')
-ADMIN_MEDIA_PREFIX = '/admin_media/'
+ADMIN_MEDIA_ROOT = os.path.join(STATIC_ROOT, 'admin')
+ADMIN_MEDIA_PREFIX = os.path.join(STATIC_URL, 'admin')
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.cache.UpdateCacheMiddleware',
