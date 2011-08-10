8575fe3bb9ec7b909e9c6fb7e2c8dcbee886af64
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-08-10 17:57:10 +0600

    Restore the accidentally removed URL for microsites

--- apps/materials/urls.py
+++ apps/materials/urls.py
@@ -113,4 +113,5 @@ urlpatterns = patterns('',
     url(r'^submit/dispatch$', "iframe_submission.iframe.dispatch_iframe", name="iframe_dispatch"),
     url(r'^submit/login$', "iframe_submission.login.login", name="iframe_login"),
     url(r'^submit/post?$', "iframe_submission.submission_form.submit", name="iframe_submission_post"),
+    url(r'^(?P<microsite>[^/]+)/?$', "microsites.microsite", name="microsite"),
 )
