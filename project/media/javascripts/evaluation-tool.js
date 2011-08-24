3bbdd45684c058a909393186cd9ce9e684b33f60
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-08-24 09:30:41 +0600

    Evaluation tool: - fix types and update copy - include Guidelines as an item into rubrics accordion - add "Previous standard" button - add link to PDF description of rubrics - fix some cross-browser compatibility issues - auto select "Superior" score and save it when rubric/tag selection is changed

--- project/media/javascripts/evaluation-tool.js
+++ project/media/javascripts/evaluation-tool.js
@@ -5,48 +5,13 @@ oer.evaluation_tool.init_rubrics = function() {
     var $rubrics = $("div.rubrics");
     var evaluate_url = $rubrics.data("evaluate-url");
     var $sections = $rubrics.children("section");
-
-    function open_section($section) {
-        $sections.filter(".expanded").removeClass("expanded").find("div.body").show().slideUp("fast");
-        if (!$section.hasClass("expanded")) {
-            $section.addClass("expanded").find("div.body").hide().slideDown("fast");
-        }
-    }
-
-    $sections.find("h1 a").click(function(e) {
-        e.preventDefault();
-        open_section($(this).closest("section"));
-    });
-
-    function select_tag($tag) {
-        if ($tag.hasClass("selected")) {
-            return;
-        }
-        var tag_id = $tag.data("tag-id");
-        $tags_ct.children(".selected").removeClass("selected");
-        $tags_ct.find("div.tag-description[data-tag-id='" + tag_id + "']").addClass("selected");
-        $tags_ct.find("div.scores[data-tag-id='" + tag_id + "']").addClass("selected");
-        $tags_ct.find("div.footer[data-tag-id='" + tag_id + "']").addClass("selected");
-        $tag.addClass("selected");
-    }
-
     var $tags_ct = $rubrics.find("div.tags");
-    $tags_ct.delegate("a.tag", "click", function(e) {
-        e.preventDefault();
-        select_tag($(this));
-    });
 
-    var $score_selectors = $rubrics.find("div.scores");
-    $score_selectors.delegate("a", "click", function(e) {
-        e.preventDefault();
-        var $this = $(this);
-        var $score = $this.closest("div");
-        if ($score.hasClass("selected")) {
+    function save_score($scores_ct) {
+        var $score = $scores_ct.find("div.selected");
+        if (!$score.length) {
             return;
         }
-        var $scores_ct = $this.closest("div.scores");
-        $scores_ct.find("div.selected").removeClass("selected");
-        $score.addClass("selected");
 
         var score_id = $score.data("score-id");
         if ($scores_ct.data("tag-id") !== undefined) {
@@ -69,8 +34,86 @@ oer.evaluation_tool.init_rubrics = function() {
             var rubric_id = $scores_ct.data("rubric-id");
             $.post(evaluate_url, {score_id: score_id, rubric_id: rubric_id});
             $scores_ct.closest("section.rubric").children("h1:first").addClass("scored");
-        } else {
         }
+        $scores_ct.data("saved", true);
+    }
+
+
+    function save_unsaved_score() {
+        // This saves scores which were selected automatically.
+
+        // Save score for currently selected alignment tag.
+        var $current_tag = $tags_ct.children(".selected");
+        var current_tag_id = $current_tag.data("tag-id");
+        var $current_tag_scores_ct = $tags_ct.find("div.scores[data-tag-id='" + current_tag_id + "']");
+        if (!$current_tag_scores_ct.data("saved")) {
+            save_score($current_tag_scores_ct);
+        }
+
+        // Save score for currently selected rubrics section
+        var $current_section_scores_ct = $sections.filter(".rubric.expanded").find("div.scores");
+        if ($current_section_scores_ct.length && !$current_section_scores_ct.data("saved")) {
+            save_score($current_section_scores_ct);
+        }
+    }
+
+    function open_section($section) {
+        save_unsaved_score();
+        $sections.filter(".expanded").removeClass("expanded").find("div.body").show().slideUp("fast");
+        if (!$section.hasClass("expanded")) {
+            $section.addClass("expanded").find("div.body").hide().slideDown("fast");
+        }
+    }
+
+    $sections.find("h1 a").click(function(e) {
+        e.preventDefault();
+        open_section($(this).closest("section"));
+    });
+
+    function select_tag($tag) {
+        if ($tag.hasClass("selected")) {
+            return;
+        }
+
+        save_unsaved_score();
+        $tags_ct.children(".selected").removeClass("selected");
+
+        var tag_id = $tag.data("tag-id");
+        $tags_ct.find("div.tag-description[data-tag-id='" + tag_id + "']").addClass("selected");
+        $tags_ct.find("div.scores[data-tag-id='" + tag_id + "']").addClass("selected");
+        $tags_ct.find("div.footer[data-tag-id='" + tag_id + "']").addClass("selected");
+        $tag.addClass("selected");
+
+        if (window.rocon != undefined) {
+            $tags_ct.find("a.tag").each(function(e, el) {
+                rocon.update(el);
+            });
+        }
+    }
+
+    $tags_ct.delegate("a.tag", "click", function(e) {
+        e.preventDefault();
+        select_tag($(this));
+    });
+
+    var $score_selectors = $rubrics.find("div.scores");
+    $score_selectors.delegate("a", "click", function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        var $score = $this.closest("div");
+        if ($score.hasClass("selected")) {
+            return;
+        }
+        var $scores_ct = $this.closest("div.scores");
+        $scores_ct.find("div.selected").removeClass("selected");
+        $score.addClass("selected");
+
+        if (window.rocon != undefined) {
+            $scores_ct.find("a").each(function(e, el) {
+                rocon.update(el);
+            });
+        }
+        save_score($scores_ct);
     });
 
     $rubrics.delegate("a.clear", "click", function(e) {
@@ -91,7 +134,7 @@ oer.evaluation_tool.init_rubrics = function() {
             $tag.removeClass("scored");
         }
         $scores_ct.find("div.selected").removeClass("selected");
-        var data = {delete: "yes"};
+        var data = {"delete": "yes"};
         if (tag_id) {
             data["tag_id"] = tag_id;
         } else if (rubric_id) {
@@ -113,6 +156,23 @@ oer.evaluation_tool.init_rubrics = function() {
         var $tag = $tags_ct.find("a.tag[data-tag-id='" + tag_id + "']").next("a.tag");
         select_tag($tag);
     });
+
+    $rubrics.delegate("a.prev-tag", "click", function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        var tag_id = $this.closest("div.footer").data("tag-id");
+        var $tag = $tags_ct.find("a.tag[data-tag-id='" + tag_id + "']").prev("a.tag");
+        select_tag($tag);
+    });
+
+    $rubrics.find("a.save").click(function(e) {
+        var $this = $(this);
+        var $section = $this.closest("section");
+        var $scores_ct = $section.find("div.scores");
+        if (!$scores_ct.data("saved")) {
+            save_score($scores_ct);
+        }
+    });
 };
 
 oer.evaluation_tool.init_evaluate_button = function() {
@@ -128,12 +188,15 @@ oer.evaluation_tool.init_evaluate_button = function() {
                 $("<iframe />", {
                     src: url,
                     width: 600,
-                    height: 650
+                    height: 700,
+                    frameBorder: 0,
+                    scrolling: "no"
                 }).appendTo($dialog);
                 $dialog.appendTo($("body"));
                 $dialog.dialog({
                     title: "Evaluate to Rubrics",
-                    width: 600
+                    width: 600,
+                    resizable: false
                 });
             } else {
                 $dialog.find("iframe").attr("src", url);
