7b4daa18eca80628d7db2c294c197fa1446797a9
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-08-25 09:09:17 +0600

    Move some global javascript code from global-init.js to global.js

--- project/media/javascripts/global-init.js
+++ project/media/javascripts/global-init.js
@@ -1,11 +1,3 @@
-IS_AUTHENTICATED = $("body").hasClass("authenticated");
-HONEYPOT_FIELD_NAME = "address";
-
-/* :econtains() filter for jQuery */
-$.expr[":"].econtains = function(obj, index, meta, stack){
-  return $.trim((obj.textContent || obj.innerText || $(obj).text() || "")).toLowerCase() == $.trim(meta[3]).toLowerCase();
-}
-
 $(function() {
     oer.search_box.init();
     if (!Modernizr.input.placeholder) {
@@ -15,7 +7,7 @@ $(function() {
             showLabelEffect : "show",
             opacity : 1
             });
-        });        
+        });
     }
 
     oer.next_url.init();
@@ -27,30 +19,4 @@ $(function() {
         });
     }
     oer.profile.init_profile_notification();
-});
-
-DEFAULT_TOOLTIP_OPTIONS = {
-    content: {
-      text: function(api) {
-        return $($(this).attr("rel"));
-      },
-      title: function(api) {
-        return $(this).text();
-      }
-    },
-    position: {
-        my: "right center",
-        at: "left center",
-      target: "event"
-    },
-    style: {
-      classes: "ui-tooltip-dark-blue ui-tooltip-shadow"
-    }
-};
-
-RIGHTSIDE_TOOLTIP_OPTIONS = $.extend(true, {}, DEFAULT_TOOLTIP_OPTIONS, {
-    position: {
-        my: "left center",
-        at: "right center"
-    }
 });
