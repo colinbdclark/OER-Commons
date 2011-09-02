e6cb56d4a3fc31ffbbf26f7472385786290a82c0
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-09-02 18:10:32 +0600

    Add update_item util function

--- apps/utils/__init__.py
+++ apps/utils/__init__.py
@@ -11,3 +11,9 @@ def reduce_whitespace(text):
     text = MULTIPLE_SPACE_RE.sub(u" ", text)
     text = MULTIPLE_NEWLINE_RE.sub(u"\n\n", text)
     return text
+
+
+def update_item(item, **kwargs):
+    item.__class__.objects.filter(pk=item.pk).update(**kwargs)
+    for key, value in kwargs.items():
+        setattr(item, key, value)
