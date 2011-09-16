1f234e2dcbd20dbecc11372aa580ec0a06270d3b
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-09-16 11:00:45 +0600

    Bugfix in weekly stat report

--- apps/stats/management/commands/send_weekly_report.py
+++ apps/stats/management/commands/send_weekly_report.py
@@ -1,12 +1,24 @@
 from django.core.management.base import BaseCommand
-from stats.report import send_report
+from optparse import make_option
 import datetime
 
 
 class Command(BaseCommand):
 
     help = u"Send a report with weekly statistics."
-    option_list = BaseCommand.option_list
+    option_list = BaseCommand.option_list + (
+        make_option('--to-date',
+            action='store',
+            type='string',
+            dest='to_date',
+            default=None,
+            help='The upper date of the report interval (ISO format). Default: None'),
+
+    )
 
     def handle(self, **options):
-        send_report(interval=datetime.timedelta(days=7))
+        from stats.report import send_report
+        to_date = options["to_date"]
+        if to_date is not None:
+            to_date = datetime.datetime.strptime(to_date, "%Y-%m-%d").date()
+        send_report(interval=datetime.timedelta(days=7), to_date=to_date)
