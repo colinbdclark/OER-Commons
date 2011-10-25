afa3c39aa55989ed36f9e6fa363751483fdb3e3d
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-10-25 20:14:18 +0600

    Replace Review form with comment form

--- apps/reviews/models.py
+++ apps/reviews/models.py
@@ -2,7 +2,9 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes import generic
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
+from haystack_scheduled.indexes import Indexed
 
 
 class Review(models.Model):
@@ -16,7 +18,7 @@ class Review(models.Model):
     content_type = models.ForeignKey(ContentType,
                                      verbose_name=_(u"Content type"))
     object_id = models.PositiveIntegerField(verbose_name=_(u"Object ID"))
-    content_object = generic.GenericForeignKey('content_type', 'object_id')
+    content_object = generic.GenericForeignKey()
 
     def __unicode__(self):
         return u"'%s' reviewed by %s" % (self.content_object, self.user)
@@ -25,3 +27,20 @@ class Review(models.Model):
         verbose_name = _(u"Review")
         verbose_name_plural = _(u"Reviews")
 
+
+#noinspection PyUnusedLocal
+@receiver(models.signals.post_save, sender=Review)
+def review_post_save(sender, **kwargs):
+    instance = kwargs["instance"]
+    content_object = instance.content_object
+    if isinstance(content_object, Indexed):
+        content_object.reindex()
+
+
+#noinspection PyUnusedLocal
+@receiver(models.signals.post_delete, sender=Review)
+def review_post_delete(sender, **kwargs):
+    instance = kwargs["instance"]
+    content_object = instance.content_object
+    if isinstance(content_object, Indexed):
+        content_object.reindex()
--- apps/reviews/views.py
+++ apps/reviews/views.py
@@ -1,99 +1,73 @@
-from annoying.decorators import JsonResponse
+from annoying.decorators import JsonResponse, ajax_request
+from annoying.functions import get_object_or_None
 from django import forms
-from django.contrib import messages
 from django.contrib.contenttypes.models import ContentType
-from django.core.urlresolvers import reverse
-from django.http import Http404
+from django.http import HttpResponseBadRequest
 from django.shortcuts import get_object_or_404
-from django.views.generic.simple import direct_to_template
-from haystack_scheduled.indexes import Indexed
+from django.template.defaultfilters import linebreaksbr
+from django.utils.decorators import method_decorator
+from django.views.generic import View
 from reviews.models import Review
 from utils.decorators import login_required
-from utils.shortcuts import redirect_to_next_url
+from utils.templatetags.utils import username
 
 
-class ReviewForm(forms.Form):
+class ReviewForm(forms.ModelForm):
 
-    text = forms.CharField(label=u"Review text:",
-                     required=False,
-                     help_text=u"You can add a review on this item to help others know more about its use or quality.",
-                     widget=forms.Textarea(attrs={"class": "text"}))
+    def clean_text(self):
+        return self.cleaned_data["text"].strip()
 
-    def __init__(self, *args, **kwargs):
-        self.instance = kwargs.pop("instance", None)
-        self.user = kwargs.pop("user", None)
-        super(ReviewForm, self).__init__(*args, **kwargs)
-
-        if self.instance is not None and self.user is not None:
-            try:
-                self.fields["text"].initial = self.instance.reviews.get(user=self.user).text
-            except Review.DoesNotExist:
-                pass
-
-    def save(self):
-        if not self.instance or not self.user:
-            return
-        content_type = ContentType.objects.get_for_model(self.instance)
-        object_id = self.instance.id
-
-        text = self.cleaned_data["text"].strip()
-
-        if not text:
-            # Delete review
-            self.instance.reviews.filter(user=self.user).delete()
-        else:
-            review, created = Review.objects.get_or_create(
-                                            content_type=content_type,
-                                            object_id=object_id,
-                                            user=self.user)
-            review.text = text
-            review.save()
-
-        if isinstance(self.instance, Indexed):
-            self.instance.reindex()
+    class Meta:
+        model = Review
+        fields = ["text"]
 
 
-@login_required
-def add(request, slug=None, model=None):
+class ReviewView(View):
 
-    if not slug or not model:
-        raise Http404()
+    #noinspection PyUnusedLocal
+    @method_decorator(login_required)
+    @method_decorator(ajax_request)
+    def post(self, request, *args, **kwargs):
+        content_type = get_object_or_404(ContentType, id=int(kwargs["content_type_id"]))
+        object_id = int(kwargs["object_id"])
+        get_object_or_404(content_type.model_class(), id=object_id)
 
-    item = get_object_or_404(model, slug=slug)
+        review = get_object_or_None(
+            Review,
+            user=request.user,
+            content_type=content_type,
+            object_id=object_id,
+        )
 
-    page_title = u"Add Review to \"%s\"" % item
-    if hasattr(item, "breadcrumbs"):
-        breadcrumbs = item.breadcrumbs()
-    else:
-        breadcrumbs = []
+        if "delete" in request.POST:
+            if not review:
+                return HttpResponseBadRequest(u"Review does not exist.")
+            else:
+                review.delete()
+                return JsonResponse(dict(
+                    status="success",
+                    message=u"Your comment was deleted.",
+                ))
 
-    breadcrumbs.append({"url": "", "title": page_title})
-
-    if request.method == "POST":
-        if "cancel" in request.POST:
-            return redirect_to_next_url(request, item.get_absolute_url())
-        form = ReviewForm(request.POST, instance=item, user=request.user)
+        if not review:
+            review = Review(
+                content_type=content_type,
+                object_id=object_id,
+                user=request.user,
+            )
+        form = ReviewForm(request.POST, instance=review)
         if form.is_valid():
-            form.save()
-            if request.is_ajax():
-                return JsonResponse(dict(status="success",
-                                         message=u"Your review was saved."))
-            else:
-                messages.success(request, u"Your review was saved.")
-                return redirect_to_next_url(request, item.get_absolute_url())
-        else:
-            if request.is_ajax():
-                errors = {}
-                for field, errors_list in form.errors.items():
-                    errors[field] = errors_list[0]
-                return JsonResponse(dict(status="error",
-                                         errors=errors))
-            else:
-                messages.error(request, u"Please correct the indicated errors.")
-    else:
-        form = ReviewForm(instance=item, user=request.user)
+            review = form.save()
+            return JsonResponse(dict(
+                status="success",
+                message=u"Your comment was saved.",
+                text=linebreaksbr(review.text),
+                author=username(review.user),
+            ))
 
-    form_action = reverse("materials:%s:add_review" % item.namespace,
-                           kwargs=dict(slug=item.slug))
-
-    return direct_to_template(request, "reviews/add.html", locals())
+        errors = {}
+        #noinspection PyUnresolvedReferences
+        for field, errors_list in form.errors.items():
+            errors[field] = errors_list[0]
+        return JsonResponse(dict(status="error",
+                                 errors=errors))
