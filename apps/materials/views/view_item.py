fff14d8e4e7b8aba143ba27348af51c347344dfa
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-11-03 19:53:37 +0600

    Sort the alignment scores on resource page from highest to lowest

--- apps/materials/views/view_item.py
+++ apps/materials/views/view_item.py
@@ -305,6 +305,7 @@ class ViewItem(BaseViewItemMixin, TemplateView):
                 score=row["score"],
                 score_class=int(round(row["score"])),
             ))
+        data["alignment_scores"].sort(key=lambda x: x["score"], reverse=True)
 
         rubric_scores = RubricScore.objects.filter(
             evaluation__confirmed=True,
