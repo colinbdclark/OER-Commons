feb1dcfdf70e67188eb285b60f77e7b20952ea7e
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-12-07 15:54:10 +0600

    Bugfix for screenshots generation script

--- apps/materials/management/commands/update_materials_screenshots.py
+++ apps/materials/management/commands/update_materials_screenshots.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.db import models
 
 
 class Command(BaseCommand):
@@ -11,15 +12,12 @@ class Command(BaseCommand):
         from materials.models.library import Library
         from materials.tasks import update_screenshot
 
-        models = [Course, Library, CommunityItem]
-
-        filters = dict(http_status=200, screenshot=None)
-
         print "Updating screenshots..."
 
-        for model in models:
-            for item in model.objects.filter(**filters):
-                update_screenshot(item)
+        for model in (Course, Library, CommunityItem):
+            for item in model.objects.filter(http_status=200).filter(
+                models.Q(screenshot=None) | models.Q(screenshot=u"")
+            ): update_screenshot(item)
 
         print "Done!"
 
