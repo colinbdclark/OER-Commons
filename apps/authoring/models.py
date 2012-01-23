2509fa8e25164bb06908c42f1024679365de357c
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2012-01-23 21:06:19 +0600

    Initial implementation of media upload tool

--- apps/authoring/models.py
+++ apps/authoring/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+import os
 
 
 class AuthoredMaterial(models.Model):
@@ -11,7 +12,21 @@ class AuthoredMaterial(models.Model):
     is_new = models.BooleanField(default=True)
 
 
+def upload_to(prefix):
+    def func(instance, filename):
+        return os.path.join("authoring", str(instance.material.id), prefix, filename)
+    return func
+
+
 class Image(models.Model):
 
-    image = models.ImageField(upload_to="authoring/images/")
     material = models.ForeignKey(AuthoredMaterial)
+
+    image = models.ImageField(upload_to=upload_to("images"))
+
+
+class Document(models.Model):
+
+    material = models.ForeignKey(AuthoredMaterial)
+
+    file = models.ImageField(upload_to=upload_to("documents"))
--- apps/authoring/urls.py
+++ apps/authoring/urls.py
@@ -1,4 +1,5 @@
 from authoring.views.edit import  Edit
+from authoring.views.media import MediaUpload
 from authoring.views.new import New
 from django.conf.urls.defaults import patterns, url
 
@@ -6,4 +7,5 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns("",
     url(r"new$", New.as_view(), name="new"),
     url(r"edit/(?P<material_id>\d+)$", Edit.as_view(), name="edit"),
+    url(r"edit/(?P<material_id>\d+)/upload$", MediaUpload.as_view(), name="upload"),
 )
