f8fe6efe155fcfa1aed1e5e064a9ce8158953b34
Anastasia Cheetham <ac@anastasiacheetham.ca>
2012-03-26 16:51:01 -0400

    FLOE-1: Cleaned up the CSS for the header.

--- project/media/javascripts/UIOptions.js
+++ project/media/javascripts/UIOptions.js
@@ -1,15 +1,3 @@
-/*
-
-Copyright 2012 OCAD University
-
-Licensed under the Educational Community License (ECL), Version 2.0 or the New
-BSD license. You may not use this file except in compliance with one these
-Licenses.
-
-You may obtain a copy of the ECL 2.0 License and BSD License at
-https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
-*/
-
 /*global jQuery, fluid*/
 
 // JSLint options 
